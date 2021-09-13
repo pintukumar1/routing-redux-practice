19ad8ce1f1c5f547810a83cf0f6a45bdba2466e6
Pintu Kumar <kumarpintu1713122@gmail.com>
2021-09-13 10:58:45 +0530

    checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,28 +91,28 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true})
-        const order = {
-            ingredients : this.state.ingredients,
-            price : this.state.totalPrice,
-            customer : {
-                name: "Pintu Kumar",
-                address: {
-                    Village : "Tikhampur",
-                    District : "Ballia",
-                    PinCode : "277001"
-                },
-                email: "kumarpintu1713122@gmail.com"
-            },
-            deliveryMethod: "fastest"
-        }
-        axios.post('/orders.json', order)
-        .then(response => {
-            this.setState({ loading: false, purchasing: false })
-        })
-        .catch(error => {
-            this.setState({ loading: false, purchasing: false })
-        });
+        // this.setState({loading: true})
+        // const order = {
+        //     ingredients : this.state.ingredients,
+        //     price : this.state.totalPrice,
+        //     customer : {
+        //         name: "Pintu Kumar",
+        //         address: {
+        //             Village : "Tikhampur",
+        //             District : "Ballia",
+        //             PinCode : "277001"
+        //         },
+        //         email: "kumarpintu1713122@gmail.com"
+        //     },
+        //     deliveryMethod: "fastest"
+        // }
+        // axios.post('/orders.json', order)
+        // .then(response => {
+        //     this.setState({ loading: false, purchasing: false })
+        // })
+        // .catch(error => {
+        //     this.setState({ loading: false, purchasing: false })
+        // });
     }
 
     render () {
